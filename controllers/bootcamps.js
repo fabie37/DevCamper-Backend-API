1dbf70fb7e2187ec928713c8f73352435e85b8d2
fabie37 <fabiecatinella@gmail.com>
2020-05-15 20:34:38 +0100

    Course Ownership

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -153,6 +153,16 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
     );
   }
 
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorised to update this bootcamp`,
+        401
+      )
+    );
+  }
+
   if (!req.files) {
     return next(new ErrorResponse(`Please upload a file`, 400));
   }
