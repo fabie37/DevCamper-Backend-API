31316e6ef6cde61956f79f26ac5f3abc64011efa
fabie37 <fabiecatinella@gmail.com>
2020-05-15 15:47:54 +0100

    User roles and authorization on crud routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -35,3 +35,18 @@ exports.protect = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 });
+
+// Grant acces to specific roles
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          `User role ${req.user.role} is not unauthorized to access this route`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
