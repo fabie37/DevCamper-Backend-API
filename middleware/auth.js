3947270d8e4cb9b33dbce440748fedbb0662f180
fabie37 <fabiecatinella@gmail.com>
2020-05-22 14:18:43 +0100

    Logout / clear cookies

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,11 +11,13 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
+    // Set token from bearer token in header
     token = req.headers.authorization.split(' ')[1];
   }
 
+  // Set token from cook
   // else if (req.cookies.token) {
-  //    token = req.cookies.token
+  //   token = req.cookies.token;
   // }
 
   // Make sure token exists
